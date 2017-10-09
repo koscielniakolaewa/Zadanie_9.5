2c5e44b1c1e2d088114d44d7774765c216fc7e7b
koscielniakolaewa <31932678+koscielniakolaewa@users.noreply.github.com>
2017-10-09 12:58:08 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var withbutton = document.getElementsByClassName('button');
+
+console.log(withbutton);
+
+var dlugosc = withbutton2.length
+
+var withbutton2 = k
+
+for ( var k = 0 ; k < 5 ; k++) {
+  console.log(function raiseAlarm() + k);
+}
