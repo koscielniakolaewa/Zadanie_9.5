c8819d9d37a55cfc267823ae5086ada72292b78f
koscielniakolaewa <31932678+koscielniakolaewa@users.noreply.github.com>
2017-10-10 15:15:01 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
 var withbutton = document.getElementsByClassName('button');
 var dlugosc = withbutton.length;
 
-var withbutton = k;
-
-for ( var k = 0 ; k < dlugosc ; k++) {
-   function raiseAlarm() {
-    alert(button);}
+for (var k = 0; k < dlugosc; k++) {
+alert(withbutton[k].innerHTML);
 }
